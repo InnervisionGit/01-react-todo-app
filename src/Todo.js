0dc52127777f9d497e9f06efb9a2fe4d39fd1a64
InnervisionGit <ivan.felbabic@gmail.com>
2020-11-15 15:01:58 +0100

    done with CRUD

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,74 @@
-import { ListItemText, ListItem, List, Button } from '@material-ui/core';
-import React from 'react';
+import {
+  ListItemText,
+  ListItem,
+  List,
+  Button,
+  Modal,
+  makeStyles,
+} from '@material-ui/core';
+import React, { useState } from 'react';
 import db from './firebase';
 import './Todo.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { SettingsPowerRounded } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props, key) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+
+  // const handleOpen = () => {
+  //   setOpen(true);
+  // };
+
+  const updateTodo = () => {
+    db.collection('todos').doc(props.todo.id).set(
+      {
+        todo: input,
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+
   return (
-    <List className='todo__list'>
-      <ListItem>
-        <ListItemText key={key.key} primary={props.todo.todo} />
-      </ListItem>
-      <Button
-        onClick={(event) => db.collection('todos').doc(props.todo.id).delete()}
-      />
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h4>Update your TODO:</h4>
+          <input
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button disabled={!input} onClick={updateTodo}>
+            Done
+          </Button>
+        </div>
+      </Modal>
+      <List className='todo__list'>
+        <ListItem>
+          <button onClick={(e) => setOpen(true)}>Edit</button>
+          <DeleteIcon
+            onClick={(event) =>
+              db.collection('todos').doc(props.todo.id).delete()
+            }
+          />
+          <ListItemText key={key.key} primary={props.todo.todo} />
+        </ListItem>
+      </List>
+    </>
   );
 }
 
